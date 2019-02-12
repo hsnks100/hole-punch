31567eb13c4366b9073ae6917d5405ac93a2fc1d
HanSoo <hsnks100@gmail.com>
2019-02-12 13:44:25 +0900

    Update server-udp.py

--- server-udp.py
+++ server-udp.py
@@ -5,9 +5,20 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 sock.bind(('', 10000))
 print("listen: ", 10000)
+addrs = []
 while True:
     data, addr = sock.recvfrom(65535)
-    print("form ip: ", addr[0], "port: ", addr[1])
+    addrs.append(addr)
+    print("from ip: ", addr[0], "port: ", addr[1])
+    sent = False
+    for x in addrs:
+        for y in addrs:
+            if x[0] != y[0]:
+                sock.sendto(y[0] + "," + str(y[1]), x)
+                sent = True
+
+    if sent == True:
+        addrs = []
     # print("echo data: ", data.decode())
     # data = str(addr[1])
     # sock.sendto(data, addr)
